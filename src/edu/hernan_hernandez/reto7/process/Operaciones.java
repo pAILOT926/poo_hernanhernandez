528ae8e126afe5e8782d9123fddb86a37116588c
Hernan Hernandez Rodriguez <hernan.hdez.rod@gmail.com>
2024-03-15 23:04:43 -0600

    Aqui esta la actualizacion del codigo de operaciones aritmeticas, el cual realiza 8 operaciones diferentes.

--- src/edu/hernan_hernandez/reto7/process/Operaciones.java
+++ src/edu/hernan_hernandez/reto7/process/Operaciones.java
@@ -1,29 +1,80 @@
 package edu.hernan_hernandez.reto7.process;
 
 public class Operaciones {
-
-    public static void suma(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     * Suma de dos nuemros
+     **/
+    public static double suma(double num1, double num2){
+        return num1 + num2;
     }
-    public static void resta(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     * Resta de dos nuemros
+     **/
+    public static double resta(double num1, double num2) {
+        return num1 - num2;
     }
-    public static void multiplicacion(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     * Multiplicacion de dos nuemros
+     **/
+    public static double multiplicacion(double num1, double num2) {
+        return num1 * num2;
     }
-    public static void division(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     * Division de dos nuemros
+     **/
+    public static double division(double num1, double num2) {
+        if (num2 != 0)
+            return num1 / num2;
+        else {
+            System.out.println("No se puede dividir por cero.");
+            return Double.NaN;
+        }
     }
-    public static void modulo(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     * Modulo de dos nuemros
+     **/
+    public static double modulo(double num1, double num2) {
+        if (num2 != 0)
+            return num1 % num2;
+        else {
+            System.out.println("No se puede calcular el módulo con cero.");
+            return Double.NaN;
+        }
     }
-    public static void potencia(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     * Potencia de dos nuemros
+     **/
+    public static double potencia(double num1, double num2) {
+        return Math.pow(num1, num2);
     }
-    public static void raiz(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     *Raiz cuadrada de un nuemro
+     **/
+    public static double raizCuadrada(double num) {
+        if (num >= 0)
+            return Math.sqrt(num);
+        else {
+            System.out.println("No se puede calcular la raíz cuadrada de un número negativo.");
+            return Double.NaN;
+        }
     }
-    public static void logaritmo(){
-        System.out.println("En desarrollo");
+    /**
+     * Esta clase sirve para realizar los siguientes calculos:
+     *Logaritmo natural de un nuemro
+     **/
+    public static double logaritmoNatural(double num) {
+        if (num > 0)
+            return Math.log(num);
+        else {
+            System.out.println("No se puede calcular el logaritmo de cero o números negativos.");
+            return Double.NaN;
+        }
     }
 }
