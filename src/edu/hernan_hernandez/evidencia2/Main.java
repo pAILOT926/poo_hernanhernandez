a74c82842678a3eb7f3dfb0ee4bb453ac4488f5c
Hernan Hernandez Rodriguez <hernan.hdez.rod@gmail.com>
2024-03-21 01:30:21 -0600

    El programa terminado sin la seleccion del idioma, en la proxima actualizacion se agregaran los idiomas.

---
+++ src/edu/hernan_hernandez/evidencia2/Main.java
@@ -0,0 +1,9 @@
+package edu.hernan_hernandez.evidencia2;
+
+import edu.hernan_hernandez.evidencia2.ui.CLITESP;
+
+public class Main {
+    public static void main(String[] args){
+        CLITESP.sesion();
+    }
+}
